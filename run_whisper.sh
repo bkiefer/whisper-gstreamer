679d4ac5b7caf27f999fa0df3960f2132bb78100
Bernd Kiefer <j_a_w_bone@gmx.de>
2024-08-20 17:14:24 +0200

    Works also on Ubuntu 24.04, in a conda env on python3.10

--- run_whisper.sh
+++ run_whisper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-LD_LIBRARY_PATH=$HOME/.local/lib/python3.10/site-packages/nvidia/cudnn/lib
+LD_LIBRARY_PATH=$HOME/local/miniconda3/envs/compat/lib/python3.10/site-packages/nvidia/cudnn/lib/
 export LD_LIBRARY_PATH
 python vadmicrowhisp.py "$@"
